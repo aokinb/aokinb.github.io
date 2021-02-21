4de3e9635a194dcf4c9e8844c4b68723a19b0830
Nicholas Aoki <nicholasaoki@Nicholass-MacBook-Air-2.local>
2021-02-21 15:15:08 -0600

    Update DigitalChalkboard.js

--- js/DigitalChalkboard.js
+++ js/DigitalChalkboard.js
@@ -52,3 +52,65 @@ function disappear() {
     let selectedElement = document.getElementById("disappear");
     selectedElement.style.display = "none";
 }
+
+
+
+
+function wikiAPI() {
+
+var searchTerm = document.getElementById('searchTerm').value;
+var connect = new XMLHttpRequest();
+var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=20&gsrsearch=" + searchTerm;
+
+connect.open('GET', url);
+
+connect.onload = function () {
+
+    var wikiObject = JSON.parse(this.response);
+
+    //wikiObject.continue.gsroffset = 30;
+
+    console.log(wikiObject);
+    console.log(wikiObject.continue.gsroffset);
+    console.log(wikiObject.query);
+    console.log(wikiObject.query.pages);
+    console.log(typeof pages);
+
+    var pages = wikiObject.query.pages;
+
+    for (var i in pages) {
+        
+        var newDiv = document.createElement("div");
+        var a = document.createElement("a");
+
+        newDiv.setAttribute('class', 'row h4');
+        document.getElementById("wiki").appendChild(newDiv);
+        document.getElementById("wiki").appendChild(a);
+        newDiv.innerText = pages[i].title;
+
+        var linkText = document.createTextNode(pages[i].title);
+        a.appendChild(linkText);
+        a.title = pages[i].title;
+        a.href = "https://en.wikipedia.org/?curid=" + pages[i].pageid;
+        
+
+        //var a = document.createElement("a");
+
+        //newDiv.appendChild(a);
+
+        //document.getElementById("wiki").appendChild(a);
+        //var linkText = document.createTextNode(pages[i].title);
+        //document.getElementById("testlink").appendChild(linkText);
+        //a.appendChild(linkText);
+        //a.title = pages[i].title;
+        //a.href = "https://en.wikipedia.org/?curid=" + pages[i].pageid;
+        //document.body.appendChild(a);
+
+        }
+
+    }
+
+connect.send();
+
+}
+
